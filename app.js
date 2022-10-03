9fc51ee13818fd53e40e9f1ba4a74681f5aa42ab
K-ash-ish <kashishsondhiya@gmail.com>
2022-10-03 11:21:35 +0530

    inserting many enteries

---
+++ app.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/peoplesDb');
+  const peopleSchema = mongoose.Schema({
+    name: String,
+    age : Number
+  });
+  const People = mongoose.model('People', peopleSchema);
+  const arr = [{name:"hello", age: 24}];
+  await People.insertMany(arr, function(err, docs){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("success");
+        console.log(docs);
+    }
+  })
+  
+//   await Peoples.save();
+  // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
+}
