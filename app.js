bc4cbc4b389563f94e0d83a83f5086f4933b3b2c
K-ash-ish <kashishsondhiya@gmail.com>
2022-10-03 11:22:08 +0530

    reading all enteries using find

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/peoplesDb');
+  await mongoose.connect("mongodb://localhost:27017/peoplesDb");
   const peopleSchema = mongoose.Schema({
     name: String,
-    age : Number
+    age: Number,
   });
-  const People = mongoose.model('People', peopleSchema);
-  const arr = [{name:"hello", age: 24}];
-  await People.insertMany(arr, function(err, docs){
-    if(err){
-        console.log(err);
+  const People = mongoose.model("People", peopleSchema);
+  await People.find({}).then(function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(docs);
     }
-    else{
-        console.log("success");
-        console.log(docs);
-    }
-  })
-  
-//   await Peoples.save();
+  });
+  //   const arr = [{name:"hello", age: 24}];
+  //   await People.insertMany(arr, function(err, docs){
+  //     if(err){
+  //         console.log(err);
+  //     }
+  //     else{
+  //         console.log("success");
+  //         console.log(docs);
+  //     }
+  //   })
+
+  //   await Peoples.save();
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
